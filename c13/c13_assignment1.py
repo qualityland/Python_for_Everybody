6698f8ebbf39bbee6be9c4761cba9e0e0439e013
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2021-10-11 17:35:46 +0200

    chapter 13 assignment done

--- c13/c13_assignment1.py
+++ c13/c13_assignment1.py
@@ -8,16 +8,16 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-#if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_1370901.xml'
-if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_1370901.xml'
+#if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
 data = urllib.request.urlopen(url, context=ctx).read()
 print('Retrieved', len(data), 'characters')
-#print(data.decode())
-tree = ET.fromstring(data)
-print(tree)
-
+stuff = ET.fromstring(data)
 # list of trees (tags)
-lst = tree.findall('comments')
-print('Comments count:', len(lst))
-# for item in lst:
-#     print('Count', item.text)
+lst = stuff.findall('comments/comment')
+print('comments count:', len(lst))
+total = 0
+for item in lst:
+    count = int(item.find('count').text)
+    total += count
+print('total:', total)
